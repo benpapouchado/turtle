af2cc9320fb10b47c96b97a9a12153d541433294
bxp830 <bxp830@cs.bham.ac.uk>
2025-10-21 13:48:28 +0100

    Hello World

---
+++ src/helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+
+    public static String helloWorld(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        String helloWorld = helloWorld();
+        System.out.println(helloWorld);
+    }
+}
