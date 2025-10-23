148e8ab866d4156b2a8197d52ed4ca8ec805be61
bxp830 <bxp830@cs.bham.ac.uk>
2025-10-23 20:41:48 +0100

    Setting up Spring boot

---
+++ src/main/java/benpapouchado/Turtle/TurtleApplication.java
@@ -0,0 +1,13 @@
+package benpapouchado.Turtle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurtleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/benpapouchado/Turtle/TurtleApplicationTests.java
@@ -0,0 +1,13 @@
+package benpapouchado.Turtle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurtleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
